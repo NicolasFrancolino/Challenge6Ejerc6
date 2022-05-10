a1890afcdd8fa0981919e5b5cf5a7a8fd8178d0e
NicolasFrancolino <87394098+NicolasFrancolino@users.noreply.github.com>
2022-05-10 13:47:25 -0300

    Agregar archivos de proyecto.

---
+++ Challenge6Ejerc6/Cola.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge6Ejerc6
+{
+    class Cola <T>
+    {
+        Queue<T> cola = new Queue<T>();
+
+        public void Encolar(T objeto)
+        {
+            cola.Enqueue(objeto);
+        }
+
+        public T Desenconlar() => cola.Dequeue();
+
+        public void MostrarCola()
+        {
+            foreach (T Objeto in cola)
+            {
+                Console.WriteLine(Objeto);
+            }
+        }
+    }
+}
---
+++ Challenge6Ejerc6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge6Ejerc6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cola<String> cola = new Cola<String>();
+            cola.Encolar("Nicolas");
+            cola.Encolar("Lucas");
+            cola.Encolar("Martina");
+            cola.Encolar("Pedro");
+            
+            cola.MostrarCola();
+            Console.WriteLine();
+            cola.Desenconlar();
+            cola.MostrarCola();
+
+            Console.ReadKey();
+        }
+    }
+}
